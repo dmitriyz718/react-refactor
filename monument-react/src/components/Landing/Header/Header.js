da0217e3ed3440d27db4cf827b95b31614966e2c
u101004132 <dzag17188@gmail.com>
2020-08-12 00:07:48 -0400

    fixed all errors that involved changing class to classname

--- monument-react/src/components/Landing/Header/Header.js
+++ monument-react/src/components/Landing/Header/Header.js
@@ -4,7 +4,8 @@ function Header() {
     return (
         <div>
             <header className="vertical-center">
-                <h1><a href="">Monument</a></h1><line></line>
+                <h1><a href="">Monument</a></h1>
+                <div></div>
                 <h2>A Lifestyle Magazine</h2>
 
             </header>
